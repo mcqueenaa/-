2d922a0aac30bda71516f82d190cfba728381222
mcqueenaa <asetorn@gmail.com>
2016-11-10 22:21:08 +0300

    третья домашка по питону

---
+++ hw5(python3)/hw3.py
@@ -0,0 +1,10 @@
+##3 вариант
+word = input('Введите слово')
+array = []
+while word:
+    array.append(word)
+    word = input('Введите слово')
+for i in range(len(array)):
+    newword = array[i]
+    newword = newword[i+1 : ]
+    print(newword)
