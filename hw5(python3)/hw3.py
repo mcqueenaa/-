d7bcdbf7201ae7b687d76fbe4caae3bb7bf987f4
mcqueenaa <asetorn@gmail.com>
2016-11-11 10:01:35 +0300

    третье дз питон

--- hw5(python3)/hw3.py
+++ hw5(python3)/hw3.py
@@ -1,9 +1,9 @@
 ##3 вариант
-word = input('Введите слово')
+word = input('Введите слово ')
 array = []
 while word:
     array.append(word)
-    word = input('Введите слово')
+    word = input('Введите слово ')
 for i in range(len(array)):
     newword = array[i]
     newword = newword[i+1 : ]
