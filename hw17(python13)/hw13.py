5b70453dd3465afcef9741ddd8f4e82987caf1e0
mcqueenaa <asetorn@gmail.com>
2017-04-13 00:54:38 +0300

    hw13

--- hw17(python13)/hw13.py
+++ hw17(python13)/hw13.py
@@ -4,11 +4,11 @@ import re
 import os
 
 def dirs():
-    kir = '[А-Яа-я]*'
+    kir = '[А-Яа-яЁё]*'
     stuff = '[A-Za-z\.\?!0-9"@№;%:?*_()-+=#$^&:;\'"><,/\|\\~`]'
     names = []
     for f in os.listdir('.'):
-        if os.path.isdir(f) and re.search(kir, f) and f not in names and re.search(stuff, f) == None:
+        if os.path.isdir(f) and re.search(kir, f) and re.search(stuff, f) == None:
             names.append(f)
     return names 
 
@@ -17,13 +17,26 @@ def answer():
     if names == []:
         print("В текущей папке нет папок, название которых состоит только из кириллических символов.")
     else:
-        print("В текущей папке " + str((len(names))) + " папок, название которых состоит только из кириллических символов. Вот их названия: ")
-        for i in names:
-            print(i)
+        print("В текущей папке " + str((len(names))) + " папок, название которых состоит только из кириллических символов.")
+
+def allfiles():
+    files = []
+    name = '.*\.'
+    for f in os.listdir('.'):
+        if os.path.isdir(f) and f not in files:
+            files.append(f)
+        elif os.path.isfile(f) and re.search(name, f):
+            n = re.search(name, f).group(0)
+            n = n.strip('.')
+            if n not in files:
+                files.append(n)
+    print("Вот названия всех найденных в текущей папке файлов и папок: ")
+    for f in files:
+        print(f)
 
 def main():
     answer()
-
+    allfiles()
 
 if __name__ == '__main__':
     main()
